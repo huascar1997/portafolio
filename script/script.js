c37005dd615ae4d1322fba333dbf18fe72fb3f09
huascar jimenez <huascarjimenez2007@gmail.com>
2022-03-14 17:43:51 +0200

    adding the scripts

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const boton = document.querySelector('.contac-me');
+const formulario = document.getElementById('formulario');
+
+boton.addEventListener('click', function(){
+     formulario.style.display = "block",
+     console.log('hi')
+})
